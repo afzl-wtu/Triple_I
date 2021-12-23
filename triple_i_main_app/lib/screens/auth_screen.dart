bbeddf86f1e387ca40fe7f23df7d77f97e811d84
Muhammad Afzal Tahir <afzl.wtu@hotmail.com>
2021-12-23 10:57:57 +0500

    Added auth

--- triple_i_main_app/lib/screens/auth_screen.dart
+++ triple_i_main_app/lib/screens/auth_screen.dart
@@ -2,46 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:main/helpers/auth_helper.dart';
 import 'package:main/screens/main_screen.dart';
 import 'package:main/widgets/backgroundGrad.dart';
 
-const users = const {
-  'dribbble@gmail.com': '12345',
-  'hunter@gmail.com': 'hunter',
-};
-
 class AuthScreen extends StatelessWidget {
+  final AuthClass _auth = AuthClass();
+
   Duration get loginTime => Duration(milliseconds: 2250);
-
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
-  }
-
-  Future<String?> _signupUser(LoginData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
-    });
-  }
-
-  Future<String> _recoverPassword(String name) {
-    debugPrint('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
-      return 'Null';
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,46 +20,18 @@ class AuthScreen extends StatelessWidget {
       background: BackgroundImage(),
       title: 'Triple I',
       logo: 'assets/images/LOGO Darmon app.png',
-      onLogin: _authUser,
-      onSignup: _signupUser,
+      onLogin: _auth.signIn,
+      onSignup: _auth.createAccount,
       loginProviders: <LoginProvider>[
         LoginProvider(
           icon: FontAwesomeIcons.google,
           label: 'Google',
-          callback: () async {
-            debugPrint('start google sign in');
-            await Future.delayed(loginTime);
-            debugPrint('stop google sign in');
-            return null;
-          },
+          callback: _auth.signWithGoogle,
         ),
         LoginProvider(
           icon: FontAwesomeIcons.facebookF,
           label: 'Facebook',
-          callback: () async {
-            debugPrint('start facebook sign in');
-            await Future.delayed(loginTime);
-            debugPrint('stop facebook sign in');
-            return null;
-          },
-        ),
-        LoginProvider(
-          icon: FontAwesomeIcons.linkedinIn,
-          callback: () async {
-            debugPrint('start linkdin sign in');
-            await Future.delayed(loginTime);
-            debugPrint('stop linkdin sign in');
-            return null;
-          },
-        ),
-        LoginProvider(
-          icon: FontAwesomeIcons.githubAlt,
-          callback: () async {
-            debugPrint('start github sign in');
-            await Future.delayed(loginTime);
-            debugPrint('stop github sign in');
-            return null;
-          },
+          callback: _auth.signInWithFacebook,
         ),
       ],
       onSubmitAnimationCompleted: () {
@@ -99,7 +39,7 @@ class AuthScreen extends StatelessWidget {
           builder: (context) => MainScreen(),
         ));
       },
-      onRecoverPassword: _recoverPassword,
+      onRecoverPassword: _auth.resetPassword,
     );
   }
 }
